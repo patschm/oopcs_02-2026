8e5daf3674f770d1f6dfdbae6adc9c95578f9b09
Patrick <pschmidt@pasit.nl>
2026-02-18 15:08:39 +0000

    day2

--- Live/M7_Surprise/TheFirm/Werknemer.cs
+++ Live/M7_Surprise/TheFirm/Werknemer.cs
@@ -1,5 +1,11 @@
 ﻿namespace TheFirm;
 
+
+// Overerving gebruik je om structureringsproblemen op te lossen
+// Ze noemen dat ook wel een is-a relatie
+// Interfaces gebruik je om interactie tussen willekeurig objecten te ontkoppelen.
+// Je erft dan ook niet van de interface, maar je implementeert hem.
+// De interface beschrijft een can-do relatie
 class Werknemer : Persoon, IContract
 {
     public void Werk()
